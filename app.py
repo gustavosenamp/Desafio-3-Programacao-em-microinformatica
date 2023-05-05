e7c9d662b262d8e928bda5c792ac62f509cf6937
Gustavo Sena <123789443+gustavosenamp@users.noreply.github.com>
2023-05-05 08:40:03 -0300

    config mysql

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, jsonify
 
-app = Flask("__name__")
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# conexão com o banco de dados
+app.config['MYSQL_Host'] = 'localhost' # 127.0.0.1
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'contatos'
+
+mysql = MySQL(app)
+
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
@@ -16,3 +26,35 @@ def contato():
 
 def quem_somos():
     return render_template('quem-somos.html')
+
+
+@app.route('/contatos', methods=['GET', 'POST'])
+def contatos():
+    if request.method == "POST":
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO contatos(email, assunto, descricao) VALUES (%s, %s, %s)", (email, assunto, descricao))
+       
+        mysql.connection.commit()
+        
+        cur.close()
+
+        return 'sucesso'
+    return render_template('contatos.html')
+
+
+# rota usuários para listar todos os usuário no banco de dados.
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
+
